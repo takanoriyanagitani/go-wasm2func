b934016e5d1d45140fd8ad7d8ed2b8d902c02c3f
yanagitani <takanori.yanagitani@gmail.com>
2024-03-08 14:05:35 +0900

    feat: compile from file path

--- compiler/compile.go
+++ compiler/compile.go
@@ -3,9 +3,23 @@ package compiler
 import (
 	"context"
 	"io"
+	"io/fs"
 )
 
 type Compiler[C any] interface {
 	Compile(ctx context.Context, wasmBytes []byte) (compiled C, e error)
 	io.Closer
+}
+
+func CompileFromPath[C any](
+	ctx context.Context,
+	compiler Compiler[C],
+	wasmPath string,
+	f fs.FS,
+) (compiled C, e error) {
+	wasmBytes, e := fs.ReadFile(f, wasmPath)
+	if nil != e {
+		return
+	}
+	return compiler.Compile(ctx, wasmBytes)
 }
